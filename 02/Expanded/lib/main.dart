51f5612af17cafc3d0ce22f2447fa7d7d915ba0e
BFlorian91 <b.florian91@icloud.com>
2020-11-24 10:42:31 +0100

    02 - Expanded

---
+++ 02/Expanded/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Expanded'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 10,
+              child: Container(
+              color: Colors.blueAccent,
+            )),
+            Expanded(
+              flex: 80,
+              child: Container(
+              color: Colors.redAccent,
+            )),
+            Expanded(
+              flex: 10,
+              child: Container(
+              color: Colors.yellowAccent,
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
