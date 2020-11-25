b7da879cf69d958a231babd74358f6bcca60d168
BFlorian91 <b.florian91@icloud.com>
2020-11-25 12:35:00 +0100

    08 floating action button

---
+++ 08/floatingActionButton/android/app/src/main/kotlin/com/example/floatingActionButton/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.floatingActionButton
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 08/floatingActionButton/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter FloatingActionButton'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => null,
+        tooltip: 'Increment',
+        child: Icon(Icons.favorite, color: Colors.red,),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(height: 50),
+        color: Colors.blue,
+        shape: CircularNotchedRectangle(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
