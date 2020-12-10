83bcf56c16adb2b8ccdcb01748bfafc6ea29ce89
BFlorian91 <b.florian91@icloud.com>
2020-12-10 15:34:58 +0100

    18-19 tooltip

---
+++ 18/customPaint/android/app/src/main/kotlin/com/example/customPaint/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.customPaint
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 19/tooltip/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              Image.network('https://img.igen.fr/2017/2/macgpic-1487794681-32564258958959-sc-jpt.jpg'),
+              Tooltip(message: 'It\'s so cool', verticalOffset: 48, height: 50, child: Icon(Icons.add),)
+            ]
+          )
+        )
+      ),
+    );
+  }
+}
