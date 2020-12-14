1691b58339fb55f7990c228c5a76467402384816
BFlorian91 <b.florian91@icloud.com>
2020-12-14 11:42:03 +0100

    init tinderLike for train

---
+++ projects/tinkerlike/android/app/src/main/kotlin/com/example/tinkerlike/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tinkerlike
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ projects/tinkerlike/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/homeScreen': (context) {
+          return ChangeNotifierProvider(create: (_) => ApiProvider(), child: HomeScreen(),)
+        }
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
