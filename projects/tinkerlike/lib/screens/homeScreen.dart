6e079ac31c3ee4696aa8dad93f05fbb5788a0b8f
BFlorian91 <b.florian91@icloud.com>
2020-12-14 16:05:30 +0100

    design from homepage in progress

--- projects/tinkerlike/lib/screens/homeScreen.dart
+++ projects/tinkerlike/lib/screens/homeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tinkerlike/widgets/bottomBar.dart';
+import 'package:tinkerlike/widgets/header.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,11 +9,38 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(child: Text('HomeView'),),
-      ),
-    );
+        body: SafeArea(
+      child: Stack(fit: StackFit.expand, children: [
+        Container(
+          decoration: BoxDecoration(color: Colors.white70),
+        ),
+        Column(
+          children: [
+            Header(),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    'assets/images/girl1.jpg',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            BottomBar()
+          ],
+        ),
+      ]),
+    ));
   }
 }
--- projects/tinkerlike/lib/screens/splashScreen.dart
+++ projects/tinkerlike/lib/screens/splashScreen.dart
@@ -10,7 +10,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 3), () {
+    Future.delayed(Duration(seconds: 5), () {
       Navigator.pushNamed(context, '/homeScreen');
     });
   }
